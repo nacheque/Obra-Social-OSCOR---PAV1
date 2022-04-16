648de1654b7cc46d9d5369b0b4fc9e4f13282313
Guillermo Rossi <guillerossi94@hotmail.com>
2022-04-16 00:57:50 -0300

    cambio posicion del formulario

--- obra-social-oscor/Form1.Designer.cs
+++ obra-social-oscor/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Escritorio del Sistema";
             this.ResumeLayout(false);
 
