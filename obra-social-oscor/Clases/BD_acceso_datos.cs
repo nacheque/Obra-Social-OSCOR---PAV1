61667e8df593a69d6dd05daacce9651479d12f06
roi-utn-08 <riodri25@gmail.com>
2022-04-26 18:49:34 -0300

    Create BD_acceso_datos.cs

---
+++ obra-social-oscor/Clases/BD_acceso_datos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obra_social_oscor.Clases
+{
+    class BD_acceso_datos
+    {
+    }
+}
