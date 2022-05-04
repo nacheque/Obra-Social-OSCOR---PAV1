bdbb78cc18021904a3d3a4d8c92cd54df916f648
roi-utn-08 <riodri25@gmail.com>
2022-05-04 14:48:36 -0300

    agrego afiliado

---
+++ obra-social-oscor/Formulario/ABM_Comunes/Afiliado.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace obra_social_oscor.Formulario.ABM_Comunes
+{
+    partial class Afiliado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Afiliado));
+            this.dtp_fecha_inscripcion = new System.Windows.Forms.DateTimePicker();
+            this.dtp_fecha_nacimiento_afiliado = new System.Windows.Forms.DateTimePicker();
+            this.txt_monto_afiliado = new System.Windows.Forms.TextBox();
+            this.lbl_monto_inscripcion = new System.Windows.Forms.Label();
+            this.lbl_fecha_inscripcion = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_salir_afiliado = new System.Windows.Forms.Button();
+            this.btn_limpiar_afiliado = new System.Windows.Forms.Button();
+            this.btn_agregar_afiliado = new System.Windows.Forms.Button();
+            this.btn_editar_afiliado = new System.Windows.Forms.Button();
+            this.btn_borrar_afiliado = new System.Windows.Forms.Button();
+            this.cmb_tipo_afiliado = new System.Windows.Forms.ComboBox();
+            this.lbl_tipo_afiliado = new System.Windows.Forms.Label();
+            this.lbl_fecha_nacimiento_afiliado = new System.Windows.Forms.Label();
+            this.txt_nombre_afiliado = new System.Windows.Forms.TextBox();
+            this.lbl_nombre_afiliado = new System.Windows.Forms.Label();
+            this.txt_apellido_afiliado = new System.Windows.Forms.TextBox();
+            this.lbl_apellido_afiliado = new System.Windows.Forms.Label();
+            this.txt_nro_afiliado = new System.Windows.Forms.TextBox();
+            this.lbl_nro_afiliado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtp_fecha_inscripcion
+            // 
+            this.dtp_fecha_inscripcion.CustomFormat = "dd/mm/yyyy h:m";
+            this.dtp_fecha_inscripcion.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_fecha_inscripcion.Location = new System.Drawing.Point(391, 66);
+            this.dtp_fecha_inscripcion.Name = "dtp_fecha_inscripcion";
+            this.dtp_fecha_inscripcion.Size = new System.Drawing.Size(120, 20);
+            this.dtp_fecha_inscripcion.TabIndex = 91;
+            // 
+            // dtp_fecha_nacimiento_afiliado
+            // 
+            this.dtp_fecha_nacimiento_afiliado.CustomFormat = "dd/mm/yyyy";
+            this.dtp_fecha_nacimiento_afiliado.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtp_fecha_nacimiento_afiliado.Location = new System.Drawing.Point(132, 93);
+            this.dtp_fecha_nacimiento_afiliado.Name = "dtp_fecha_nacimiento_afiliado";
+            this.dtp_fecha_nacimiento_afiliado.Size = new System.Drawing.Size(120, 20);
+            this.dtp_fecha_nacimiento_afiliado.TabIndex = 90;
+            // 
+            // txt_monto_afiliado
+            // 
+            this.txt_monto_afiliado.Location = new System.Drawing.Point(392, 90);
+            this.txt_monto_afiliado.Name = "txt_monto_afiliado";
+            this.txt_monto_afiliado.Size = new System.Drawing.Size(43, 20);
+            this.txt_monto_afiliado.TabIndex = 89;
+            // 
+            // lbl_monto_inscripcion
+            // 
+            this.lbl_monto_inscripcion.AutoSize = true;
+            this.lbl_monto_inscripcion.Location = new System.Drawing.Point(293, 93);
+            this.lbl_monto_inscripcion.Name = "lbl_monto_inscripcion";
+            this.lbl_monto_inscripcion.Size = new System.Drawing.Size(91, 13);
+            this.lbl_monto_inscripcion.TabIndex = 88;
+            this.lbl_monto_inscripcion.Text = "Monto Inscripcion";
+            // 
+            // lbl_fecha_inscripcion
+            // 
+            this.lbl_fecha_inscripcion.AutoSize = true;
+            this.lbl_fecha_inscripcion.Location = new System.Drawing.Point(279, 69);
+            this.lbl_fecha_inscripcion.Name = "lbl_fecha_inscripcion";
+            this.lbl_fecha_inscripcion.Size = new System.Drawing.Size(106, 13);
+            this.lbl_fecha_inscripcion.TabIndex = 87;
+            this.lbl_fecha_inscripcion.Text = "Fecha de Inscripcion";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(19, 118);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(765, 263);
+            this.dataGridView1.TabIndex = 86;
+            // 
+            // btn_salir_afiliado
+            // 
+            this.btn_salir_afiliado.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_salir_afiliado.BackgroundImage")));
+            this.btn_salir_afiliado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_salir_afiliado.Location = new System.Drawing.Point(734, 387);
+            this.btn_salir_afiliado.Name = "btn_salir_afiliado";
+            this.btn_salir_afiliado.Size = new System.Drawing.Size(50, 50);
+            this.btn_salir_afiliado.TabIndex = 85;
+            this.btn_salir_afiliado.UseVisualStyleBackColor = true;
+            // 
+            // btn_limpiar_afiliado
+            // 
+            this.btn_limpiar_afiliado.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_limpiar_afiliado.BackgroundImage")));
+            this.btn_limpiar_afiliado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_limpiar_afiliado.Location = new System.Drawing.Point(734, 58);
+            this.btn_limpiar_afiliado.Name = "btn_limpiar_afiliado";
+            this.btn_limpiar_afiliado.Size = new System.Drawing.Size(50, 50);
+            this.btn_limpiar_afiliado.TabIndex = 84;
+            this.btn_limpiar_afiliado.UseVisualStyleBackColor = true;
+            // 
+            // btn_agregar_afiliado
+            // 
+            this.btn_agregar_afiliado.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_agregar_afiliado.BackgroundImage")));
+            this.btn_agregar_afiliado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_agregar_afiliado.Location = new System.Drawing.Point(19, 387);
+            this.btn_agregar_afiliado.Name = "btn_agregar_afiliado";
+            this.btn_agregar_afiliado.Size = new System.Drawing.Size(50, 50);
+            this.btn_agregar_afiliado.TabIndex = 83;
+            this.btn_agregar_afiliado.UseVisualStyleBackColor = true;
+            // 
+            // btn_editar_afiliado
+            // 
+            this.btn_editar_afiliado.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_editar_afiliado.BackgroundImage")));
+            this.btn_editar_afiliado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_editar_afiliado.Location = new System.Drawing.Point(75, 387);
+            this.btn_editar_afiliado.Name = "btn_editar_afiliado";
+            this.btn_editar_afiliado.Size = new System.Drawing.Size(50, 50);
+            this.btn_editar_afiliado.TabIndex = 82;
+            this.btn_editar_afiliado.UseVisualStyleBackColor = true;
+            // 
+            // btn_borrar_afiliado
+            // 
+            this.btn_borrar_afiliado.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_borrar_afiliado.BackgroundImage")));
+            this.btn_borrar_afiliado.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_borrar_afiliado.Location = new System.Drawing.Point(131, 387);
+            this.btn_borrar_afiliado.Name = "btn_borrar_afiliado";
+            this.btn_borrar_afiliado.Size = new System.Drawing.Size(50, 50);
+            this.btn_borrar_afiliado.TabIndex = 81;
+            this.btn_borrar_afiliado.UseVisualStyleBackColor = true;
+            // 
+            // cmb_tipo_afiliado
+            // 
+            this.cmb_tipo_afiliado.FormattingEnabled = true;
+            this.cmb_tipo_afiliado.Location = new System.Drawing.Point(391, 40);
+            this.cmb_tipo_afiliado.Name = "cmb_tipo_afiliado";
+            this.cmb_tipo_afiliado.Size = new System.Drawing.Size(121, 21);
+            this.cmb_tipo_afiliado.TabIndex = 80;
+            // 
+            // lbl_tipo_afiliado
+            // 
+            this.lbl_tipo_afiliado.AutoSize = true;
+            this.lbl_tipo_afiliado.Location = new System.Drawing.Point(319, 43);
+            this.lbl_tipo_afiliado.Name = "lbl_tipo_afiliado";
+            this.lbl_tipo_afiliado.Size = new System.Drawing.Size(65, 13);
+            this.lbl_tipo_afiliado.TabIndex = 79;
+            this.lbl_tipo_afiliado.Text = "Tipo Afiliado";
+            // 
+            // lbl_fecha_nacimiento_afiliado
+            // 
+            this.lbl_fecha_nacimiento_afiliado.AutoSize = true;
+            this.lbl_fecha_nacimiento_afiliado.Location = new System.Drawing.Point(17, 95);
+            this.lbl_fecha_nacimiento_afiliado.Name = "lbl_fecha_nacimiento_afiliado";
+            this.lbl_fecha_nacimiento_afiliado.Size = new System.Drawing.Size(108, 13);
+            this.lbl_fecha_nacimiento_afiliado.TabIndex = 78;
+            this.lbl_fecha_nacimiento_afiliado.Text = "Fecha de Nacimiento";
+            // 
+            // txt_nombre_afiliado
+            // 
+            this.txt_nombre_afiliado.Location = new System.Drawing.Point(131, 66);
+            this.txt_nombre_afiliado.Name = "txt_nombre_afiliado";
+            this.txt_nombre_afiliado.Size = new System.Drawing.Size(121, 20);
+            this.txt_nombre_afiliado.TabIndex = 77;
+            // 
+            // lbl_nombre_afiliado
+            // 
+            this.lbl_nombre_afiliado.AutoSize = true;
+            this.lbl_nombre_afiliado.Location = new System.Drawing.Point(81, 69);
+            this.lbl_nombre_afiliado.Name = "lbl_nombre_afiliado";
+            this.lbl_nombre_afiliado.Size = new System.Drawing.Size(44, 13);
+            this.lbl_nombre_afiliado.TabIndex = 76;
+            this.lbl_nombre_afiliado.Text = "Nombre";
+            // 
+            // txt_apellido_afiliado
+            // 
+            this.txt_apellido_afiliado.Location = new System.Drawing.Point(131, 40);
+            this.txt_apellido_afiliado.Name = "txt_apellido_afiliado";
+            this.txt_apellido_afiliado.Size = new System.Drawing.Size(121, 20);
+            this.txt_apellido_afiliado.TabIndex = 75;
+            // 
+            // lbl_apellido_afiliado
+            // 
+            this.lbl_apellido_afiliado.AutoSize = true;
+            this.lbl_apellido_afiliado.Location = new System.Drawing.Point(81, 43);
+            this.lbl_apellido_afiliado.Name = "lbl_apellido_afiliado";
+            this.lbl_apellido_afiliado.Size = new System.Drawing.Size(44, 13);
+            this.lbl_apellido_afiliado.TabIndex = 74;
+            this.lbl_apellido_afiliado.Text = "Apellido";
+            // 
+            // txt_nro_afiliado
+            // 
+            this.txt_nro_afiliado.Location = new System.Drawing.Point(131, 14);
+            this.txt_nro_afiliado.Name = "txt_nro_afiliado";
+            this.txt_nro_afiliado.Size = new System.Drawing.Size(43, 20);
+            this.txt_nro_afiliado.TabIndex = 73;
+            // 
+            // lbl_nro_afiliado
+            // 
+            this.lbl_nro_afiliado.AutoSize = true;
+            this.lbl_nro_afiliado.Location = new System.Drawing.Point(26, 17);
+            this.lbl_nro_afiliado.Name = "lbl_nro_afiliado";
+            this.lbl_nro_afiliado.Size = new System.Drawing.Size(99, 13);
+            this.lbl_nro_afiliado.TabIndex = 72;
+            this.lbl_nro_afiliado.Text = "Numero de  Afiliado";
+            // 
+            // Afiliado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dtp_fecha_inscripcion);
+            this.Controls.Add(this.dtp_fecha_nacimiento_afiliado);
+            this.Controls.Add(this.txt_monto_afiliado);
+            this.Controls.Add(this.lbl_monto_inscripcion);
+            this.Controls.Add(this.lbl_fecha_inscripcion);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_salir_afiliado);
+            this.Controls.Add(this.btn_limpiar_afiliado);
+            this.Controls.Add(this.btn_agregar_afiliado);
+            this.Controls.Add(this.btn_editar_afiliado);
+            this.Controls.Add(this.btn_borrar_afiliado);
+            this.Controls.Add(this.cmb_tipo_afiliado);
+            this.Controls.Add(this.lbl_tipo_afiliado);
+            this.Controls.Add(this.lbl_fecha_nacimiento_afiliado);
+            this.Controls.Add(this.txt_nombre_afiliado);
+            this.Controls.Add(this.lbl_nombre_afiliado);
+            this.Controls.Add(this.txt_apellido_afiliado);
+            this.Controls.Add(this.lbl_apellido_afiliado);
+            this.Controls.Add(this.txt_nro_afiliado);
+            this.Controls.Add(this.lbl_nro_afiliado);
+            this.Name = "Afiliado";
+            this.Text = "Afiliado";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtp_fecha_inscripcion;
+        private System.Windows.Forms.DateTimePicker dtp_fecha_nacimiento_afiliado;
+        private System.Windows.Forms.TextBox txt_monto_afiliado;
+        private System.Windows.Forms.Label lbl_monto_inscripcion;
+        private System.Windows.Forms.Label lbl_fecha_inscripcion;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_salir_afiliado;
+        private System.Windows.Forms.Button btn_limpiar_afiliado;
+        private System.Windows.Forms.Button btn_agregar_afiliado;
+        private System.Windows.Forms.Button btn_editar_afiliado;
+        private System.Windows.Forms.Button btn_borrar_afiliado;
+        private System.Windows.Forms.ComboBox cmb_tipo_afiliado;
+        private System.Windows.Forms.Label lbl_tipo_afiliado;
+        private System.Windows.Forms.Label lbl_fecha_nacimiento_afiliado;
+        private System.Windows.Forms.TextBox txt_nombre_afiliado;
+        private System.Windows.Forms.Label lbl_nombre_afiliado;
+        private System.Windows.Forms.TextBox txt_apellido_afiliado;
+        private System.Windows.Forms.Label lbl_apellido_afiliado;
+        private System.Windows.Forms.TextBox txt_nro_afiliado;
+        private System.Windows.Forms.Label lbl_nro_afiliado;
+    }
+}
