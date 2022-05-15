5b3e4ec510eec51e7f3b5d664ef0eee31c667f08
Exagorth <andres.claro.cordoba@gmail.com>
2022-05-15 17:26:08 -0300

    AD-PRACTICAS

---
+++ obra-social-oscor/AccesoADatos/AD_Practica.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obra_social_oscor.AccesoADatos
+{
+    public class AD_Practica
+    {
+        public static DataTable obtenerPracticas()
+        {
+            string cadenaDeConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaDeConexion);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT * FROM PRACTICAS";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
---
+++ obra-social-oscor/Entidades/Practica.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obra_social_oscor.Entidades
+{
+    public class Practica
+    {
+        private string descripcion;
+        private string precio; //Modificar tipo de dato una vez verificada la funcionalidad
+
+
+        public Practica(string descripcionPractica, string precioPractica)
+        {
+            this.descripcion = descripcionPractica;
+            this.precio = precioPractica;
+        }
+
+        public string DescripcionPractica
+        {
+            get => descripcion;
+            set => descripcion = value;
+        }
+
+        public string PrecioPractica
+        {
+            get => precio;
+            set => precio = value;
+        }
+    }
+    
+}
