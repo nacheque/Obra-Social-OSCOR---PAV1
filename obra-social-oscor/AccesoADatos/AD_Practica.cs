f47717de15016fd0cb26aa77c324e3b186655758
roi-utn-08 <riodri25@gmail.com>
2022-05-17 18:25:27 -0300

    Create AD_Practica.cs
    solo es para tener combo, pero la de pollo tienen todo

---
+++ obra-social-oscor/AccesoADatos/AD_Practica.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace obra_social_oscor.AccesoADatos
+{
+    class AD_Practica
+    {
+        public static DataTable obtenerComboPracticas()
+        {
+            string cadenaDeConexion = System.Configuration.ConfigurationManager.AppSettings["CadenaBD"];
+            SqlConnection cn = new SqlConnection(cadenaDeConexion);
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                string consulta = "SELECT P.ID_PRACTICA, P.DESCRIPCION FROM PRACTICAS P";
+
+                cmd.Parameters.Clear();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = consulta;
+
+                cn.Open();
+                cmd.Connection = cn;
+
+                DataTable tabla = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(tabla);
+
+                return tabla;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                cn.Close();
+            }
+        }
+    }
+}
