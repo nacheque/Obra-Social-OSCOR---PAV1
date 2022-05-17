0c8bdd1a86fbf787a7b1b54b9324e9a73b45b8d2
roi-utn-08 <riodri25@gmail.com>
2022-05-17 18:24:13 -0300

    Create Practica.cs
    solo copie la de pollo, pero no esta completo todocuando pollo suba su rama debemos modificar todo esto, veo como hago

---
+++ obra-social-oscor/Entidades/Practica.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obra_social_oscor.Entidades
+{
+    class Practica
+    {
+        private int codigoPractica;
+        private string descripcion;
+        private float precio; //Modificar tipo de dato una vez verificada la funcionalidad
+
+
+        public Practica(string descripcionPractica, float precioPractica)
+        {
+            this.descripcion = descripcionPractica;
+            this.precio = precioPractica;
+        }
+
+        public Practica()
+        {
+
+        }
+
+        public int CodigoPractica
+        {
+            get => codigoPractica;
+            set => codigoPractica = value;
+        }
+        public string DescripcionPractica
+        {
+            get => descripcion;
+            set => descripcion = value;
+        }
+
+        public float PrecioPractica
+        {
+            get => precio;
+            set => precio = value;
+        }
+    }
+
+}
+
